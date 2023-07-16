2b6880422895b4caf772d909f6fd551324105925
DavideSberna <sbernadavide@gmail.com>
2023-07-17 00:28:10 +0200

    complete branch

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -38,10 +38,8 @@
                   <div class="card rounded-4 rounded">
                     <div class="card-body">
                       <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input">
-                        <!-- <input type="checkbox" class="form-check-input" :value="all"> -->
-                        <!-- <input type="checkbox" class="form-check-input" :checked="isAllChecked" :value="all" @click="resetCeckBox()"> -->
-                        <label class="form-check-label">all</label>
+                        <input type="checkbox" class="form-check-input inputt" :checked="filterCategoryNames.length < 1" @click="resetCheckBox($event)" id="resetCheckbox">
+                        <label class="form-check-label">Tutti</label>
                       </div>
                       <div class="mb-3 form-check" v-for="category in categories" :key="category.id">
                         <input type="checkbox" class="form-check-input" :id="category.id" :value="category.name" @click="clickCheckBox(category.name)">
@@ -112,7 +110,7 @@
                         </div>
                       </router-link>
                     </div>
-                    <div v-if="!showNotFound() && filterCategoryNames.length == 0" class="d-flex justify-content-center mt-5 mb-5">
+                    <div v-if="!showNotFound() && filterCategoryNames.length == 0 && inputText == ''" class="d-flex justify-content-center mt-5 mb-5">
                       <nav aria-label="Page navigation example my-3">
                         <ul class="pagination">
                           <li class="page-item"><button class="page-link" :class="{ 'disabled': currentPage === 1 }" @click="getData(currentPage - 1)">Previous</button></li>
@@ -123,54 +121,6 @@
                           <li class="page-item"><button class="page-link" :class="{ 'disabled': currentPage === lastPage }" @click="getData(currentPage + 1)">Next</button></li>
                         </ul>
                       </nav>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-8 col-lg-9 col-xl-10" v-if="1 > 2">
-              <div class="row ps-2 pe-2 mb-3">
-
-                <div class="col-12">
-                  <h3 class="">Ristoranti filtrati</h3>
-                  <span v-for="category in checkedCategories" :key="category" class="me-2 badge rounded-pill shadow-sm text-bg-dark rounded badge-style">{{ category }}</span>
-                </div>
-              </div>
-              <div class="row ps-2 pe-2 mb-5" :class="{ 'opacity-0': loading }">
-                <p class="fs-4 mb-3">Le tue scelte</p>
-                <div class="col-sm-6 col-lg-4 col-xl-3 pb-4" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
-                  <router-link class="link-offset-2 link-underline link-underline-opacity-0" :to="{ name: 'restaurant-show', params: { slug: restaurant.slug } }">
-                    <div class="card bg-light border border-0">
-                      <img class="w-100 rounded-4 img-restaurant shadow rounded" :src="'http://localhost:8000/storage/' + restaurant.image" :alt="restaurant.name">
-                      <div class="pt-2">
-                        <p class="m-0 ps-2">{{ restaurant.name }}</p>
-                        <div class="pt-1">
-                          <span v-for="category in restaurant.categories" :key="category" class="me-2 badge rounded-pill text-body-tertiary shadow-sm bg-body-tertiary rounded">{{
-                            category }}</span>
-                        </div>
-                      </div>
-                    </div>
-                  </router-link>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-8 col-lg-9 col-xl-10" v-if="notFound">
-              <div class="row ps-2 pe-2 mb-3">
-                <div class="col-12">
-                  <h3 class="">Ristoranti filtrati</h3>
-                  <span v-for="category in checkedCategories" :key="category" class="me-2 badge rounded-pill shadow-sm text-bg-dark rounded badge-style">{{ category }}</span>
-                </div>
-              </div>
-              <div class="row ps-2 pe-2 mb-5 mt-5 pt-5 pb-5">
-                <div class="col-sm-5 col-lg-5 col-xl-3 pb-4 m-auto">
-                  <div class="card bg-light border border-0">
-                    <div class="pt-2">
-                      <p class="m-0 ps-2 text-center">Ci dispiace, non sono stati trovati ristoranti con le categorie
-                        selezionate</p>
-                      <div class="pt-1 text-center mt-3">
-                        <a class="btn btn-primary" href="#" role="button" @click="resetCeckBox()">Inzia da capo</a>
-                      </div>
                     </div>
                   </div>
                 </div>
@@ -221,8 +171,6 @@ export default {
   },
   methods: {
     getData(page) {
-
-
       let params = {
         'page': page
       };
@@ -234,157 +182,25 @@ export default {
         this.categories = res.data.results.categories;
         this.restaurants = res.data.results.restaurants.data;
         this.restaurantsAll = res.data.results.restaurantsAll;
-        // this.clickCheckBox();
-        // this.getRestaurantsRanked(this.restaurantsAll)
-        // console.log(this.restaurants);
+
       }).catch((error) => {
         console.log(error);
       }).finally(() => {
         this.loading = false;
         this.notFound = false
         this.filteredRestaurants = []
-
-        if(this.store.categorySelected) {
-
-      this.filterCategoryNames.push(this.store.categorySelected);
-      // let checkboxes = document.querySelectorAll("input[type='checkbox']");
-      // console.log(checkboxes + "endiowjiodqwio");
-      // for(let i = 0; i < checkboxes.length; i++)
-      //     checkboxes[i].checked = true;
-    }
+        if (this.store.categorySelected) {
+          this.filterCategoryNames.push(this.store.categorySelected);
+          console.log(this.filterCategoryNames)
+          console.log(this.filterCategoryNames.length)
+        }
       })
-
-      // if (page != undefined) {
-      //   this.showPopular = false
-      //   this.isAllChecked = true
-      // }
-      // if (page == 1) {
-      //   this.showPopular = true
-      //   this.isAllChecked = true
-      // }
-
-
     },
 
-    // resetCeckBox() {
-    //   this.notFound = false
-    //   this.isAllChecked = true;
-    //   this.checkedCategories = [];
-    //   this.filteredRestaurants = [];
-    //   let checkboxprova = document.querySelectorAll('input[type="checkbox"]')
-    //   checkboxprova.forEach((item, index) => {
-
-    //     if (index == 0) {
-    //       item.checked = true;
-    //       this.isAllChecked = true
-    //       this.filteredRestaurants = []
-    //     } else {
-    //       item.checked = false;
-    //       this.isAllChecked = false
-    //     }
-    //   })
-    // },
-
-    // clickCheckBox(value) {
-    //   if (this.index === 0 && this.categoryId) {
-    //     if (this.categoryId == 1) {
-    //       this.name = 'Italiano'
-    //     } else if (this.categoryId == 2) {
-    //       this.name = 'Pizza'
-    //     } else if (this.categoryId == 3) {
-    //       this.name = 'Cinese'
-    //     } else if (this.categoryId == 4) {
-    //       this.name = 'Hamburger'
-    //     } else {
-    //       this.name = 'All'
-    //       this.index = 2;
-    //     }
-    //     if (this.name !== 'All') {
-    //       this.checkedCategories.push(this.name);
-    //     }
-    //     this.index++;
-    //     this.filteredRestaurant();
-    //   } else {
-    //     if (this.checkedCategories.includes(value)) {
-    //       const index = this.checkedCategories.indexOf(value);
-    //       this.checkedCategories.splice(index, 1);
-    //     } else {
-    //       this.checkedCategories.push(value);
-    //     }
-
-    //     if (this.checkedCategories.length > 0) {
-    //       let checkboxprova = document.querySelectorAll('input[type="checkbox"]')
-    //       checkboxprova[0].checked = false
-    //       this.isAllChecked = false;
-    //     } else {
-    //       this.isAllChecked = true;
-    //       let checkboxprova = document.querySelectorAll('input[type="checkbox"]')
-    //       checkboxprova[0].checked = true
-    //       this.filteredRestaurants = [];
-    //     }
-    //     this.index++;
-    //     this.filteredRestaurant();
-    //   }
-    // },
-
-    // filteredRestaurant() {
-    //   if (this.checkedCategories.length > 0) {
-    //     this.filteredRestaurants = this.restaurantsAll.filter(restaurante =>
-    //       this.checkedCategories.every(category => restaurante.categories.includes(category))
-    //     );
-    //     this.notFound = this.filteredRestaurants.length === 0;
-    //   } else {
-    //     this.filteredRestaurants = [];
-    //     this.notFound = false;
-    //   }
-
-    // },
-
-    // filterSearch() {
-    //   if (this.nameRestaurants != '') {
-    //     let checkboxprova = document.querySelectorAll('input[type="checkbox"]')
-    //     checkboxprova.forEach(item => {
-    //       if (item.checked) {
-    //         item.checked = false
-    //         this.checkedCategories = []
-    //       }
-    //     })
-    //     this.nameRestaurants = this.nameRestaurants.toLowerCase()
-    //     this.filteredRestaurants = []
-    //     this.restaurantsAll.forEach(restaurant => {
-    //       let name = restaurant.name.toLowerCase()
-    //       if (name.includes(this.nameRestaurants)) {
-    //         this.filteredRestaurants.push(restaurant)
-    //       }
-    //     })
-    //   } else {
-    //     this.isAllChecked = true
-    //     this.filteredRestaurants = []
-    //     let checkboxprova = document.querySelectorAll('input[type="checkbox"]')
-    //     checkboxprova.forEach(item => {
-    //       console.log(item.value)
-    //       if (item.value == 'all') {
-    //         item.checked = true
-    //       } else {
-    //         item.checked = false
-    //       }
-    //     })
-
-    //   }
-    // },
-
-    // firstFilter() {
-    //   this.filteredRestaurants = this.restaurants.filter(restaurant => {
-    //     return restaurant.categories.includes(this.name);
-    //   });
-    // },
-
-    // getRestaurantsRanked(data) {
-    //   data.sort((a, b) => b.total_orders - a.total_orders);
-    //   this.restaurantsRanked = data.slice(0, 7);
-    // },
+    
 
     resetCheckBox(event) {
+      
       let checkboxes = document.querySelectorAll("input[type='checkbox']");
       for (let i = 0; i < checkboxes.length; i++) {
         checkboxes[i].checked = false;
@@ -393,24 +209,6 @@ export default {
       event.currentTarget.checked = true;
       this.filterCategoryNames = [];
       this.filteredRestaurants = [];
-    },
-
-    clickCheckBox(categoryName) {
-
-      document.getElementById("resetCheckbox").checked = false;
-
-      if (!this.filterCategoryNames.includes(categoryName))
-        this.filterCategoryNames.push(categoryName);
-      else
-        this.filterCategoryNames = this.filterCategoryNames.filter((name) => name != categoryName);
-
-      if (this.filterCategoryNames.length > 0) {
-        this.filteredRestaurants = this.restaurantsAll.filter((restaurant) => {
-        return restaurant.categories.some((category) => this.filterCategoryNames.includes(category));
-      });
-      }
-      return this.filteredRestaurants
-
     },
 
     showRestaurant(restaurantCategories) {
@@ -424,25 +222,55 @@ export default {
       return result;
     },
 
-    searchByName() {
+    clickCheckBox(categoryName) {
+      this.inputText = ''
+      document.getElementById("resetCheckbox").checked = false;
 
-      if (this.inputText != '') {
-        this.filteredRestaurants = []
-        this.inputText = this.inputText.toUpperCase()
-        this.restaurantsAll.forEach(restaurant => {
-          let name = restaurant.name.toUpperCase()
-          if (name.includes(this.inputText)) {
-            this.filteredRestaurants.push(restaurant)
-          }
-        })
+      if (!this.filterCategoryNames.includes(categoryName)) {
+        this.filterCategoryNames.push(categoryName);
+      } else {
+        this.filterCategoryNames = this.filterCategoryNames.filter((name) => name !== categoryName);
       }
-      return this.filteredRestaurants
+
+      if (this.filterCategoryNames.length > 0) {
+        this.filteredRestaurants = this.restaurantsAll.filter((restaurant) => {
+          return this.filterCategoryNames.every((category) => restaurant.categories.includes(category))
+            && restaurant.categories.length === this.filterCategoryNames.length;
+        });
+      } else {
+        this.filteredRestaurants = [];
+      }
+      return this.filteredRestaurants;
     },
+
+
+  
+    searchByName() {
+      if (this.inputText !== '') {
+        this.resetCheckBox(event);
+        this.filteredRestaurants = [];
+
+        document.getElementById("resetCheckbox").checked = true;
+        this.inputText = this.inputText.toUpperCase();
+        
+        this.restaurantsAll.forEach((restaurant) => {
+          let name = restaurant.name.toUpperCase();
+          if (name.startsWith(this.inputText)) {
+            this.filteredRestaurants.push(restaurant);
+          }
+        });
+      } else {
+        document.getElementById("resetCheckbox").checked = true;
+        this.filteredRestaurants = [];
+      }
+      return this.filteredRestaurants;
+    },
+
 
     categoryChecked(categoryName) {
       let result = false;
 
-      if(this.filterCategoryNames.includes(categoryName))
+      if (this.filterCategoryNames.includes(categoryName))
         result = true;
 
       return result;
@@ -453,17 +281,9 @@ export default {
 
       if ((this.inputText != '' || this.filterCategoryNames.length > 0) && this.filteredRestaurants.length < 1) {
         result = true;
+        document.getElementById("resetCheckbox").checked = true;
       }
-      console.log(result);
-
       return result;
-      // let result = false;
-
-      // for(let i = 0; i < this.restaurants.length && result; i++)
-      //   if(this.searchByName(this.restaurants[i].name) && this.showRestaurant(this.restaurants[i].categories))
-      //     result = false;
-
-      // return result;
     }
   },
   computed: {
